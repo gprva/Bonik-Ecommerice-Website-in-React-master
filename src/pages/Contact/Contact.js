55f6fbb3c6404fd407d9585f014ad918fc599a5b
gprva <148754074+gprva@users.noreply.github.com>
2024-05-20 15:55:31 +0600

    added pages contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,9 +5,21 @@ import "./Contact.css";
 const About = () => {
   return (
     <>
-      <section >
+      <section className='contact_main'>
       <div className="container">
-            <h2 className='delivery_tit'>Контакты</h2>
+            <h2 className='contact_tit'>Вы можете связаться с нами</h2>
+      </div>
+    </section>
+
+    <section>
+      <div className="container">
+
+        <p className="contact_phon">Контактный тел: +996 505 505 400</p>
+        <p className="contact_phon">Соц. сети</p>
+        <p className="contact_phon"> <span>Whatsapp:</span> +996 505 505 400</p>
+        <p className="contact_phon"> <span>Telegram:</span> +996 505 505 400  </p>
+        <p className="contact_phon"> <span>Instagram:</span> gprva_</p>
+        
       </div>
     </section>
     </>
